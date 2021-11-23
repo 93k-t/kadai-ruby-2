fb00e01dcb8ee9e1979399ee19a9fc69f1d53500
Ubuntu <ubuntu@ip-172-31-4-99.ap-northeast-1.compute.internal>
2021-11-23 07:15:35 +0000

    kadai-ruby-2

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,17 +1,21 @@
 def fizzbuzz(num_max)
+# メソッド内とメソッド呼び出し時に puts を複数記述すると戻り値に nil(空行) が追加される
   if num_max % 15 == 0
-        puts "FizzBuzz"
+        num_max = "FizzBuzz"
   elsif num_max % 5 == 0
-        puts "Buzz"
+        num_max = "Buzz"
   elsif  num_max % 3 == 0
-        puts "Fizz"
+        num_max = "Fizz"
   else
-        puts num_max.to_s
+        num_max = num_max.to_s
   end
 end
+# 戻り値を num_max に代入
+
+num_max = 100
 
 (1..100).each do |num_max|
-  fizzbuzz(num_max)
+  puts fizzbuzz(num_max)
 end
 
 # puts fizzbuzz(1)
